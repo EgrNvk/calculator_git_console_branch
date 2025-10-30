c2162455c8454beab6ba16fe78f3803819d8505c
EgrNvk <104683701+EgrNvk@users.noreply.github.com>
2025-10-30 14:30:43 +0200

    FRONTEND Add main code

--- calculator_git_console_branch.py
+++ calculator_git_console_branch.py
@@ -26,3 +26,39 @@ class Calculator:
             result = self.number1 / self.number2
             print(f"{self.number1} / {self.number2} = {result}")
             return
+
+while True:
+    number1=input("Введіть перше число - ")
+    if number1.isalpha():
+        print("Некоректний ввод. Введіть число")
+        continue
+    else:
+        number1=float(number1)
+        break
+
+while True:
+    number2=input("Введіть друге число - ")
+    if number2.isalpha():
+        print("Некоректний ввод. Введіть число")
+        continue
+    else:
+        number2=float(number2)
+        break
+
+while True:
+    action=input("Введіть потрібну дію (+, -, *, /) ")
+    if action == "+":
+        Calculator(number1,number2).addition()
+        break
+    elif action == "-":
+        Calculator(number1,number2).subtraction()
+        break
+    elif action == "*":
+        Calculator(number1,number2).multiplication()
+        break
+    elif action == "/":
+        Calculator(number1,number2).division()
+        break
+    else:
+        print("Некоректний ввод. Виберіть потрібну дію (+, -, *, /) ")
+        continue
