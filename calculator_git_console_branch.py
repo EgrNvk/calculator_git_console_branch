eb3288849d921686b9e03922c2329c2afef0f8d7
EgrNvk <104683701+EgrNvk@users.noreply.github.com>
2025-10-30 14:23:41 +0200

    BACKEND Add functions addition, subtraction, multiplication, division

--- calculator_git_console_branch.py
+++ calculator_git_console_branch.py
@@ -2,3 +2,27 @@ class Calculator:
     def __init__(self, number1, number2):
         self.number1 = number1
         self.number2 = number2
+
+    def addition(self):
+        result = self.number1 + self.number2
+        print(f"{self.number1} + {self.number2} = {result}")
+        return
+
+    def subtraction(self):
+        result = self.number1 - self.number2
+        print(f"{self.number1} - {self.number2} = {result}")
+        return
+
+    def multiplication(self):
+        result = self.number1 * self.number2
+        print(f"{self.number1} * {self.number2} = {result}")
+        return
+
+    def division(self):
+        if self.number2 == 0:
+            print("На 0 ділити не можна")
+            return
+        else:
+            result = self.number1 / self.number2
+            print(f"{self.number1} / {self.number2} = {result}")
+            return
